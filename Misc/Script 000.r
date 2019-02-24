b1f8b7755ed4d2d8766d6816f2d0a5c46116555c
rmncruz <rui.miguel.cruz@gmail.com>
2019-02-24 17:23:59 +0000

    commit R

---
+++ Misc/Script 000.r
@@ -0,0 +1,21 @@
+
+
+a <- c(9, 16, 25)
+b <- sqrt(a)
+b
+
+
+for (i in 1:10) { 
+    print(i**2) 
+}
+
+
+# An example of computing the mean with variables
+# mydata$sum <- mydata$x1 + mydata$x2
+# mydata$mean <- (mydata$x1 + mydata$x2) / 2
+
+
+ds <- c(1.5, 6.7, 8.9) * 1:12
+ds
+
+mean(ds)
